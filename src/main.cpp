6448326759688f06c254c004f853be9551e8f5c7
Connor Mackay <44244596+connorwmackay@users.noreply.github.com>
2024-09-26 15:54:21 +0100

    Added Debug Output

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,35 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-int main() {
+void GLAPIENTRY MessageCallback( GLenum source,
+                 GLenum type,
+                 GLuint id,
+                 GLenum severity,
+                 GLsizei length,
+                 const GLchar* message,
+                 const void* userParam )
+{
+    fprintf( stderr, "GL CALLBACK: %s type = 0x%x, severity = 0x%x, message = %s\n",
+            ( type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "" ),
+            type, severity, message );
+}
+
+
+int main(int argc, char **argv) {
     Font::init();
     Font font = Font("assets/MinimalPixelFont.ttf", 16);
 
     if (!glfwInit())
         return -1;
-
+    
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+
+    #ifdef _DEBUG
+        std::cout << "Running in Debug Mode\n";
+        glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+    #endif
 
     GLFWwindow* window = glfwCreateWindow(1280, 720, "Text Renderer", nullptr, nullptr);
 
@@ -28,6 +47,11 @@ int main() {
     glViewport(0, 0, 1280, 720);
     glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 
+    #ifdef _DEBUG
+        glEnable(GL_DEBUG_OUTPUT);
+        glDebugMessageCallback(MessageCallback, 0);
+    #endif
+
     glEnable(GL_BLEND);
     glEnable(GL_DEPTH_TEST);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
@@ -40,7 +64,6 @@ int main() {
 
         glfwSwapBuffers(window);
     }
-    
 
     glfwDestroyWindow(window);
     glfwTerminate();
